4736b514bc3f33aeb1455e0fb788338233701108
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-06-24 15:18:39 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (89.27%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,8 +1,18 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return i;
+        int start=0;
+        int end=nums.length-1;
+        int mid;
+        while(start<=end){
+            mid=(start+end)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
             }
         }
         return -1;
