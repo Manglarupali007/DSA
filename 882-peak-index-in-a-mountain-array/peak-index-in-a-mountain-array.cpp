735decc239f2b9ad487b68848256e1a07fabbefe
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-07-29 21:02:44 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (78.40%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -5,14 +5,16 @@ public:
              int start=0;
              int end=n-1;
              while(start<end){
-                int mid=start+(end-start)/2;
+                // int mid=start+(end-start)/2;
+                int mid=(start+end)/2;
                 if(arr[mid]<arr[mid+1]){
                     start=mid+1;
                 }
                 else{
                     end=mid;
                 }
-                mid=start+(end-start)/2;
+                // mid=start+(end-start)/2;
+                  mid=(start+end)/2;
              }
              return start;
     }
