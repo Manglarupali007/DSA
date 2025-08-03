beb79c3e05de390e8428872b6e37a6e1c2c1a7d0
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-08-03 09:44:46 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (9.77%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.java
+++ 319-bulb-switcher/bulb-switcher.java
@@ -1,19 +1,5 @@
 class Solution {
     public int bulbSwitch(int n) {
-        int i=0;
-        int j=1;
-        if(n==0){
-            return 0;
-        }
-        else if(n<=3){
-            return j;
-        }
-        while(i<n){
-            i+=2*j+1;
-            if(i<n){
-                j++;
-            }
-        }
-        return j;
+         return (int)Math.sqrt(n);
     }
 }
