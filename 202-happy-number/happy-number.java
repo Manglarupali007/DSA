9a7f2693633a52fec06cd38a9b5e5d0576315230
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-10-12 14:38:32 +0530

    Time: 2 ms (16.79%) | Memory: 40.7 MB (75.92%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -2,17 +2,17 @@ class Solution {
     public boolean isHappy(int n) {
         HashSet<Integer> set = new HashSet<>();
         while(n!=1){
-            set.add(n);
-            n = findSquare(n);
             if(set.contains(n)){
                 return false;
             }
+            set.add(n);
+            n=square(n);
         }
         return true;
     }
-    public int findSquare(int n){
+    public int square(int n){
         int sum = 0;
-        while(n!=0){
+        while(n>0){
             int digit = n%10;
             sum += digit*digit;
             n/=10;
