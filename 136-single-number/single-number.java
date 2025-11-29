abfef3f0d74219d25c3077e2bc05c9c34ad334e4
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-11-29 20:10:42 +0530

    Time: 1 ms (99.89%) | Memory: 46.9 MB (15.98%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int res=0;
         for(int i=0;i<nums.length;i++){
-            res^=nums[i];
+            res^=nums[i]; //XOR with same number results in 0 and XOR with 0  will replace with number itself;
         }
         return res;
     }
