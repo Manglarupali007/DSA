b5353acf838386631c33c993c2229df8e8d74402
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-12-02 13:00:05 +0530

    Time: 11 ms (14.90%) | Memory: 47 MB (5.06%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverseWords(String s) {
+        s=s.trim();
+        String word[]=s.split("\\s+");
+        String ans="";
+        for(int i=word.length-1;i>=0;i--){
+            ans+=word[i];
+            if(i>0){
+                ans+=" ";
+            }
+        }
+        return ans;
+    }
+}
