ad698186cd1c896cf61615c461f3aa5da15ee5b7
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-12-05 17:46:03 +0530

    Time: 5 ms (71.18%) | Memory: 46.2 MB (6.04%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int temp=x;
         int rev=0;
+        int temp=x;
         while(temp>0){
             int rem=temp%10;
             rev=rev*10+rem;
