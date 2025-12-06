dfb71c595e0fc8eaf746b9be5dd8ee7cada37f1a
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-12-06 19:55:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (9.45%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -10,13 +10,12 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-      if (head == null || head.next == null)
+         if(head == null || head.next == null){
             return head;
-  ListNode temp=head.next;
-         head.next=swapPairs(temp.next);
-      
-       temp.next=head;
-    
-    return temp;
+        }
+        ListNode temp=head.next;
+        head.next=swapPairs(head.next.next);
+        temp.next=head;
+        return temp;
     }
 }
