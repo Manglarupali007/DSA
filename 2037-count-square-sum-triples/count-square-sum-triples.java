1b9ba71a98f847b8c38489dc706f36f00967512f
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-12-08 17:57:23 +0530

    Time: 13 ms (56.16%) | Memory: 41.6 MB (67.22%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -3,7 +3,8 @@ class Solution {
         int count=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                long sqr=1L*(i*i)+1L*(j*j);
+                // long sqr=1L*(i*i)+1L*(j*j);
+                long sqr=(i*i)+(j*j); 
                 double root=Math.sqrt(sqr);
                 int k=(int)root;
                 if(k*k==sqr && k<=n){
