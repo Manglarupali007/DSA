2fb5251cc3dd96f2decc358feb225df58076b5cc
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-12-10 14:09:20 +0530

    Time: 4 ms (9.81%) | Memory: 44.1 MB (6.32%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,9 +1,7 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int [] merged=new int [m+n];
-        int k=0;
-        int i=0;
-        int j=0;
+        int k=0, i=0, j=0;
         while(i<m){
             merged[k++]=nums1[i++];
         }
