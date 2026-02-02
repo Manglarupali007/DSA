8c7dca325606471eddce21becd60574a13f8d3bc
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-02-02 13:03:20 +0530

    Time: 14 ms (32.50%) | Memory: 46.3 MB (31.69%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,23 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase();
-        s=s.replaceAll("[^a-z0-9]","");
-        // int n=s.length();
-        // int i=0, j=n-1;
-        // while(i<=j){
-        //     if(s.charAt(i)!=s.charAt(j)){
-        //         return false;
-        //     }
-        //     i++;
-        //     j--;
-        // }
-        // return true;
-
-        StringBuilder sb=new StringBuilder(s);
-        String rev=sb.reverse().toString();
-        if(s.equals(rev)){
-            return true;
+s=s.toLowerCase().replaceAll("[^a-z0-9]","");
+        int i=0;
+        int j=s.length()-1;
+       while(i<=j){
+        if(s.charAt(i)!=s.charAt(j)){
+            return false;
         }
-        return false;
+        i++;
+        j--;
     }
+    return true;
+}
 }
