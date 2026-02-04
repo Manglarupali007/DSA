fb0c2c02285e3253652feb7e1a2b58de43e8caba
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-02-04 10:21:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (37.93%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,17 +1,28 @@
 class Solution {
-    public void sortColors(int[] nums) {
-         int n=nums.length;
-        for(int i=0;i<n-1;i++){
-            int min=i;
-            for(int j=i+1;j<n;j++){
-                if(nums[j]<nums[min]){
-                  min=j;
-                }
-                
-            }
-        int temp=nums[i];
-                nums[i]=nums[min];
-                nums[min]=temp;
+    static void quicksort(int arr[], int low, int high){
+        if(low<high){
+            int p=partition(arr, low, high);
+            quicksort(arr, low, p-1);
+            quicksort(arr, p+1, high);
         }
+    }
+    static int partition(int arr[], int low, int high){
+int pivot=arr[high];
+int i=low-1;
+for(int j=low;j<high;j++){
+    if(arr[j]<pivot){
+        i++;
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
+int temp=arr[i+1];
+arr[i+1]=arr[high];
+arr[high]=temp;
+return i+1;
+    }
+    public void sortColors(int[] nums) {
+        quicksort(nums,0, nums.length-1);
     }
 }
