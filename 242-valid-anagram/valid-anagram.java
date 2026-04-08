7ec527354a88b9e5cc7972e5dd72124bd142a722
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-04-08 20:28:51 +0530

    Time: 5 ms (61.36%) | Memory: 44.4 MB (69.60%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,9 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[]srr=s.toCharArray();
-          char[]trr=t.toCharArray();
-        Arrays.sort(srr);
-        Arrays.sort(trr);
-        return Arrays.equals(srr,trr);
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int [] count=new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)- 'a']++;
+             count[t.charAt(i)- 'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
