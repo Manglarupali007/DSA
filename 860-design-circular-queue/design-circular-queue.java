acc6d10b665875bec00e49f77e0d0c8c3e8b7a3c
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-04-15 15:02:04 +0530

    Time: 5 ms (35.08%) | Memory: 46.5 MB (55.42%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.java
+++ 860-design-circular-queue/design-circular-queue.java
@@ -1,56 +1,61 @@
 class MyCircularQueue {
 int queue[];
-int front, rear, SIZE;
+int front;
+int rear;
+int size;
     public MyCircularQueue(int k) {
-        queue=new int[k];
-        SIZE=k;
+        size=k;
         front=-1;
         rear=-1;
+        queue=new int[k];
     }
     
     public boolean enQueue(int value) {
-        if((rear+1)%SIZE == front){
+        if((rear+1)%size == front){
             return false;
         }
         if(front==-1){
             front=0;
         }
-        rear=(rear+1)%SIZE;
-queue[rear]=value;
-return true;
+        rear=(rear+1)%size;
+        queue[rear]=value;
+        return true;
     }
     
     public boolean deQueue() {
         if(front==-1){
             return false;
         }
-        int result=queue[front];
         if(front==rear){
             front=-1;
             rear=-1;
         }
         else{
-            front=(front+1)%SIZE;
+front=(front+1)%size;
         }
         return true;
     }
     
     public int Front() {
-        if (front == -1) return -1;
+        if(front==-1){
+            return -1;
+        }
         return queue[front];
     }
     
     public int Rear() {
-        if (rear == -1) return -1;
+        if(rear==-1){
+            return -1;
+        }
         return queue[rear];
     }
     
     public boolean isEmpty() {
-        return front == -1;
+        return front==-1;
     }
     
     public boolean isFull() {
-        return (rear + 1) % SIZE == front;
+        return (rear+1)%size==front;
     }
 }
 
