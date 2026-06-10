6d6c501648d2b1794593d30f8d164d88cf718285
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-06-10 19:39:51 +0530

    Time: 1909 ms (5.07%) | Memory: 55.2 MB (70.52%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,41 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // int n=nums.length;
-        // int majority=n/2;
-        // for(int i=0;i<n;i++){
-        //     int count=0;
-        //     for(int j=0;j<n;j++){
-        //         if(nums[i]==nums[j]){
-        //             count++;
-        //         }
-        //     }
-        //     if(count>majority){
-        //         return nums[i];
-        //     }
-        // }
-        // return -1;
-
-
-
-
         int n=nums.length;
-        int candidate=0;
         int count=0;
         for(int i=0;i<n;i++){
-            if(count==0){
-                candidate=nums[i];
+            count=0;
+            for(int j=0;j<n;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
             }
-            if(candidate==nums[i]){
-                count++;
-            }
-            else{
-                count--;
-            }
+            if(count>n/2) 
+            return nums[i];
         }
-        return candidate;
-
-
-
-
+        return -1;
     }
 }
