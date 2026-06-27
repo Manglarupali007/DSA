d828f0ea2cfa93c8a122af893a7605eb16103e93
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-06-27 15:06:10 +0530

    Time: 2 ms (91.97%) | Memory: 47.7 MB (75.13%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -14,3 +14,28 @@ class Solution {
         }
     }
 }
+
+
+
+
+
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int n = nums.length;
+//         int[] temp = new int[n];
+//         int index = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] != 0) {
+//                 temp[index] = nums[i];
+//                 index++;
+//             }
+//         }
+//         while (index < n) {
+//             temp[index] = 0;
+//             index++;
+//         }
+//         for (int i = 0; i < n; i++) {
+//             nums[i] = temp[i];
+//         }
+//     }
+// }
