324187b61845b87b09b3eeddf82886332ed49ceb
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-06-27 14:48:47 +0530

    Time: 0 ms (100.00%) | Memory: 61.1 MB (69.71%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -12,7 +12,8 @@ class Solution {
         int n=nums.length;
         k=k%n;
         reverse(nums, 0 , n-1);
-        reverse(nums, 0 , k-1);
+       
         reverse(nums, k, n-1);
+         reverse(nums, 0 , k-1);
     }
 }
