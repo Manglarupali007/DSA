da5076b63b3826b78c9443ea9f4dccd8aa8da05a
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-07-06 21:42:40 +0530

    Time: 1 ms (94.05%) | Memory: 43.3 MB (69.90%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,22 +1,17 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new ArrayList<>();
-
-        for (int i = 0; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>();
-            int n = 1;
-
-            for (int j = 0; j <= i; j++) {
-                row.add(n);
-                // formula for next value in row
-                n = n * (i - j) / (j + 1);
+            List<List<Integer>> ans = new ArrayList<>(); // add every row to this
+            for(int i=0;i<numRows;i++){
+                     List<Integer> temp = new ArrayList<>(); //for every row 
+                     long res=1;
+                     temp.add(1);
+                     for(int j=0;j<i;j++){
+                        res*=(i-j);
+                        res/=(j+1);
+                        temp.add((int)res);
+                     }
+                     ans.add(temp);
             }
-
-            result.add(row);
-        }
-
-        return result;
+            return ans;
     }
 }
