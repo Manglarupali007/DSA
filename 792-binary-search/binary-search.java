bb2eb83f24e69e2892018d2e0b73704c7e880491
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-07-11 12:09:08 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (31.13%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -6,7 +6,7 @@ class Solution {
         while(low<=high){
             int mid=(high+low)/2;
             if(nums[mid]==target) return mid;
-            else if(target>nums[mid]) low=mid+1;
+            else if(nums[mid]<target) low=mid+1;
             else high=mid-1;
         }
         return -1;
