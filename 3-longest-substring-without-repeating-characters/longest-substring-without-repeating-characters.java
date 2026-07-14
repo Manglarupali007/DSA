921e6467aff553d655bb3fdc226588988123ca1b
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-07-14 12:12:47 +0530

    Time: 7 ms (37.50%) | Memory: 45.4 MB (81.57%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,23 +3,17 @@ class Solution {
         int n = s.length();
         int left = 0;
         int max = 0;
-
         HashMap<Character, Integer> map = new HashMap<>();
-
         for (int right = 0; right < n; right++) {
-
             map.put(s.charAt(right),
                     map.getOrDefault(s.charAt(right), 0) + 1);
-
             while (map.get(s.charAt(right)) > 1) {
                 map.put(s.charAt(left),
                         map.get(s.charAt(left)) - 1);
                 left++;
             }
-
             max = Math.max(max, right - left + 1);
         }
-
         return max;
     }
 }
