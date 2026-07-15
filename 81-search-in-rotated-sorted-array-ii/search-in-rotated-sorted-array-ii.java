5d108bebcf036aa99fc9be819f8a0b04cfecf680
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-07-15 19:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (65.25%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return true;
-            }
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==target) return true;
         }
         return false;
     }
