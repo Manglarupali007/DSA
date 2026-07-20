033261629c3173466d7cd3cd987c992a588035f5
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-07-20 19:59:28 +0530

    Time: 10 ms (17.67%) | Memory: 46.9 MB (13.37%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,10 +1,11 @@
 class Solution {
     public String reverseWords(String s) {
         String arr[]=s.split("\\s+");
-        StringBuilder ans = new StringBuilder();
+        // StringBuilder ans = new StringBuilder();
+        String ans="";
         int n=arr.length;
         for(int i=n-1;i>=0;i--){
-            ans.append(arr[i]+" ");
+            ans+=(arr[i]+" ");
         }
         return ans.toString().trim();
     }
