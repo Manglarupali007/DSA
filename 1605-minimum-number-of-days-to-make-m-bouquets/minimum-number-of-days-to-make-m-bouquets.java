f7441a15001bca768df5d17d166bb1e7db27b698
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-07-21 19:31:07 +0530

    Time: 15 ms (97.47%) | Memory: 82.9 MB (12.65%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,33 +1,31 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        int low = Integer.MAX_VALUE;
-        int high = Integer.MIN_VALUE;
+        int low=Integer.MAX_VALUE;
+        int high=Integer.MIN_VALUE;
         int n=bloomDay.length;
-        if ((long)m * k > n) return -1;
-        for (int i=0;i<n;i++) {
-            low = Math.min(low, bloomDay[i]);
-            high = Math.max(high, bloomDay[i]);
+        if((long)m*k>n) return -1;
+        for(int i=0;i<n;i++){
+            low=Math.min(low,bloomDay[i]);
+            high=Math.max(high,bloomDay[i]);
         }
-        while (low <= high) {
-            int mid = (high+low)/2;
-            int bouquet = 0;
-            int count = 0;
-            for (int day : bloomDay) {
-                if (day <= mid) {
+        while(low<=high){
+            int mid=(high+low)/2;
+            int bou=0;
+            int count=0;
+            for(int day:bloomDay){
+                if(day<=mid){
                     count++;
-                    if (count == k) {
-                        bouquet++;
-                        count = 0;
+                    if(count==k){
+                        bou++;
+                        count=0;
                     }
-                } else {
-                    count = 0;
+                }
+                else{
+                    count=0;
                 }
             }
-            if (bouquet >= m) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+            if(bou>=m) high=mid-1;
+            else low=mid+1;
         }
         return low;
     }
