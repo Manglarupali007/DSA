3aa05448d55d47c6fbe8cd94983458469af79b6c
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-07-21 10:05:31 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (91.82%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -8,33 +8,24 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) {
-            return head;
-        }
-
-        ListNode tail = head;
-        int n = 1;
-        while (tail.next != null) {
-            tail = tail.next;
+        if(head==null) return null;
+        ListNode tail=head;
+        int n=1;
+        while(tail.next!=null){
+            tail=tail.next;
             n++;
         }
-
-        k = k % n;
-        if (k == 0) return head;
-
-        tail.next = head; // make circle
-
-        int steps = n - k;
-        ListNode newTail = head;
-       for (int i = 1; i < steps; i++) {
-            newTail = newTail.next;
+        k=k%n;
+        tail.next=head;
+        int steps=n-k-1;
+        ListNode newtail=head;
+        while(steps-->0){
+            newtail=newtail.next;
         }
-        ListNode newHead = newTail.next;
-        newTail.next = null; 
-
-        return newHead;
+        ListNode newhead=newtail.next;
+        newtail.next=null;
+        return newhead;
     }
 }
