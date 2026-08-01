1a21c098794df2c68cc9b0413ada467a08efe9c9
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-08-01 20:12:33 +0530

    Time: 14 ms (29.50%) | Memory: 47.5 MB (12.41%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,18 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
 s=s.toLowerCase().replaceAll("[^a-z0-9]","");
-if(s.length()==0){
-    return true;
-}
-        int i=0;
-        int j=s.length()-1;
-       while(i<=j){
-        if(s.charAt(i)!=s.charAt(j)){
-            return false;
-        }
-        i++;
-        j--;
-    }
-    return true;
+if(s.length()==0) return true;
+StringBuilder sb=new StringBuilder(s);
+return sb.reverse().toString().equals(s);
 }
 }
