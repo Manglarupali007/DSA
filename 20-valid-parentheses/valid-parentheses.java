20d2d19120166a3b6052a60aa7f57925a009125a
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-08-01 21:54:37 +0530

    Time: 3 ms (86.05%) | Memory: 43.3 MB (58.24%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st=new Stack<>();
         int n=s.length();
-        if(n%2!=0) return false;
+        // if(n%2!=0) return false;
         for(int i=0;i<n;i++){
             char c=s.charAt(i);
             if(c=='[' || c=='(' || c=='{')   st.push(c);
