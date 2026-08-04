6b933f5d985b5a3579dac564ae1a0debd0c739cd
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-08-04 12:18:12 +0530

    Time: 1 ms (97.37%) | Memory: 47.9 MB (12.26%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -12,12 +12,10 @@ class Solution {
                 }
             }
         }
-for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-        if(row[i]==1||col[j]==1){
-            matrix[i][j]=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i]==1 || col[j]==1) matrix[i][j]=0;
+            }
         }
-    }
-}
     }
 }
