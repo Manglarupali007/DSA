14d24b64ee9a063338c0665083b4ca7ce88b4b24
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-08-20 12:28:44 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (34.04%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,14 +11,13 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast=head;
+        if(head==null) return false;
         ListNode slow=head;
+        ListNode fast=head;
         while(fast!=null && fast.next!=null){
-            fast=fast.next.next;
             slow=slow.next;
-            if(slow==fast){
-                return true;
-            }
+            fast=fast.next.next;
+            if(slow==fast) return true;
         }
         return false;
     }
