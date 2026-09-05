c0ab440101ff3f06695437dd30cdb1b9ecb030af
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-09-05 11:44:10 +0530

    Time: 2626 ms (5.11%) | Memory: 51 MB (42.99%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -9,5 +9,20 @@ class Solution {
             }
         }
         return count;
+
+
+
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // map.put(0,1);
+        // int count=0;
+        // int sum=0;
+        // int n=nums.length;
+        // for(int i=0;i<n;i++){
+        //     sum+=nums[i];
+        //     int reqd=sum-goal;
+        //     if(map.containsKey(reqd)) count+=map.get(reqd);
+        //     map.put(sum,map.getOrDefault(sum,0)+1);
+        // }
+        // return count;
     }
 }
