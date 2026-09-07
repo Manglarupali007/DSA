4eeb2d84961fe99211371e86cab99f33e82bfc2d
Rupali Mangla <rupalimangla.9842@gmail.com>
2026-09-07 18:29:30 +0530

    Time: 1 ms (77.69%) | Memory: 42 MB (97.46%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,17 +1,18 @@
 class Solution {
     public int get(int n){
-        int dig=0;
+        if(n==0)return 0;
+        int sum=0;
         while(n>0){
-int rem=n%10;
-dig+=rem*rem;
-n/=10;
+            int dig=n%10;
+            sum+=dig*dig;
+            n/=10;
         }
-        return dig;
+        return sum;
     }
     public boolean isHappy(int n) {
         int slow=n;
         int fast=get(n);
-        while(fast!=1 && fast!=slow){
+        while(fast!= 1 && slow!=fast){
             slow=get(slow);
             fast=get(get(fast));
         }
